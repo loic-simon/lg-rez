f9f35f2a8998997ae260c1bbc79dc85c469b3451
Loïc Simon <loic.pano@gmail.com>
2020-05-16 20:42:30 +0200

    Bot Discord : structuration plus claire et fonctionnelle

--- Discord/annexe.py
+++ Discord/annexe.py
@@ -1,3 +1,14 @@
 
 def test(ctx):
-    return f"{ctx.message.content} ? {ctx.message.content} !!!"
+    arg = ctx.message.content.split(maxsplit=1)[1]  # enlève le !test du début
+    auteur = ctx.author.name
+    salon = ctx.channel.name if hasattr(ctx.channel, "name") else f"DMChannel de {ctx.channel.recipient.name}"
+    serveur = ctx.guild.name if hasattr(ctx.guild, "name") else "(DM)"
+    # pref = ctx.prefix
+    # com = ctx.command
+    # ivkw = ctx.invoked_with
+    return (f"```arg = {arg}\n"
+            f"auteur = {auteur}\n"
+            f"salon = {salon}\n"
+            f"serveur = {serveur}```"
+           )
--- Discord/bot.py
+++ Discord/bot.py
@@ -7,57 +7,68 @@ from dotenv import load_dotenv
 import annexe
 import IA
 
-
+# Récupération du token du bot et de l'ID du serveur
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = int(os.getenv("DISCORD_GUILD_ID"))
 
-bot = commands.Bot(command_prefix='!')
+
+# Création du bot
+COMMAND_PREFIX = "!"
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, description="Bonjour")
 
 
+# Trigger au démarrage du bot
 @bot.event
 async def on_ready():
     print(f"{bot.user} has connected to Discord!")
 
     guild = bot.get_guild(GUILD_ID)
 
-    print(
-        f"{bot.user} is connected to the following guild:\n"
-        f"{guild.name} (id: {guild.id})\n"
+    print(f"{bot.user} connecté au serveur « {guild.name} » (id : {guild.id})\n"
     )
 
     members = "\n - ".join([member.name for member in guild.members])
     print(f"Guild Members:\n - {members}")
 
     channels = "\n - ".join([channel.name for channel in guild.channels if isinstance(channel, discord.TextChannel)])
-    print(f"Channels:\n - {channels}")
+    print(f"\nChannels:\n - {channels}")
 
 
+# Trigger à l'arrivée d'un membre sur le serveur
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(
-        f"Hi {member.name}, welcome to my Discord server!"
-    )
+    await member.dm_channel.send(f"Hi {member.name}, welcome to my Discord server!")
 
 
+# Trigger à chaque message
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:           # Sécurité pour éviter les boucles infinies
+    if message.author == bot.user:          # Sécurité pour éviter les boucles infinies
         return
-
-    rep = IA.main(message.content)
     
-    if rep:                     # Si l'IA a un truc à dire
-        await message.channel.send(rep)
+    await bot.process_commands(message)     # On trigger toutes les commandes
+
+    if not message.content.startswith(COMMAND_PREFIX):      # Si pas une commande (+ conditions sur les channels ? à venir), on appelle l'IA
+        rep = IA.main(message.content)
+
+        if rep:                     # Si l'IA a un truc à dire
+            await message.channel.send(rep)
 
 
+# Commandes définies dans les fichiers annexes !
 @bot.command(name="test")
-async def holder_test(ctx):
+async def test(ctx):
     rep = annexe.test(ctx)
     await ctx.send(rep)
 
 
-bot.run(TOKEN)
+# Trigger si erreur dans une commande
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"{type(error).__name__}: {str(error)}")
 
-print("salut")
+
+# Exécute le tout (bloquant, rien n'es exécuté après)
+bot.run(TOKEN)
