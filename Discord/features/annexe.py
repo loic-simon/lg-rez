02ae692a039d96686e299b1bce878d634f42d8bf
Calwik <pe.besdeberc@gmail.com>
2020-05-18 15:01:18 +0200

    C'est un test

--- Discord/features/annexe.py
+++ Discord/features/annexe.py
@@ -9,7 +9,7 @@ class Annexe(commands.Cog):
     @commands.has_role("MJ")
     async def test(self, ctx, *, arg):
         """Test : test !"""
-        
+
         # arg = tools.command_arg(ctx)    # Arguments de la commande (sans le !test) --> en fait c'est faisable nativement, zrtYes
         auteur = ctx.author.display_name
         salon = ctx.channel.name if hasattr(ctx.channel, "name") else f"DMChannel de {ctx.channel.recipient.name}"
@@ -17,32 +17,32 @@ class Annexe(commands.Cog):
         # pref = ctx.prefix
         # com = ctx.command
         # ivkw = ctx.invoked_with
-        
+
         await tools.log(ctx, "Alors, ça log ?")
-        
+
         await ctx.send(tools.code_bloc(
             f"arg = {arg}\n"
             f"auteur = {auteur}\n"
             f"salon = {salon}\n"
             f"serveur = {serveur}"
         ))
-        
-        
+
+
     @commands.command()
-    @commands.has_role("MJ")    
+    @commands.has_role("MJ")
     async def testbdd(self, ctx):
         """Test BDD"""
-        
+
         tous = cache_TDB.query.all()
         ret = '\n - '.join([u.nom for u in tous])
         await ctx.send(tools.code_bloc(f"Liste des joueurs :\n - {ret}"))
-        
-        
+
+
     @commands.command()
     @commands.has_role("MJ")
     async def rename(self, ctx, id: int, nom: str):
         """Renommer quelqu'un à partir de son ID"""
-        
+
         try:
             u = cache_TDB.query.filter_by(messenger_user_id=id).one()
         except:
@@ -52,3 +52,4 @@ class Annexe(commands.Cog):
             u.nom = nom
             db.session.commit()
             await ctx.send(tools.code_bloc(f"Joueur {oldnom} renommé en {nom}."))
+        #ceci est un testbdd
