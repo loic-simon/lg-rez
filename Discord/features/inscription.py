4c889499d922ef328cd1e1847259aa64f049f6bd
Tom Lacoma <lacoma.tom@gmail.com>
2020-05-18 16:54:58 +0200

    Débuggué, mais pas au bon endroit

--- Discord/features/inscription.py
+++ Discord/features/inscription.py
@@ -3,13 +3,14 @@ import tools
 from bdd_connect import db, cache_TDB
 
 async def main(bot, member):
-    for chan in member.guild.channels:
-        if chan.topic == f"{member.id}":
-            await member.dm_channel.send(f"Tu as déjà un channel à ton nom, {member.name}")
-            return
+    if chan:=tools.get(member.guild.text_channels, topic=f"{member.id}"):
+        await chan.send(f"Tu as déjà un channel à ton nom, {member.mention}")
+        return
 
-    chan = await guild.create_text_channel(f"perso-{member.name}", category = "CONVERSATION BOT", topic=f"{member.id}") #Crée le channel "perso-nom" avec le topic "member.id"
+    print(f"{member.id}")
 
-    await chan.set_permissions(member, read_messages = True, write_messages=True)
+    chan = await member.guild.create_text_channel(f"conv-bot-{member.name}", category = tools.get(member.guild.channels, name="CONVERSATION BOT"), topic=f"{member.id}") #Crée le channel "perso-nom" avec le topic "member.id"
 
-    chan.send(f"yousk2 {member.name}")
+    await chan.set_permissions(member, read_messages = True, send_messages=True)
+
+    await chan.send(f"yousk2 {member.mention}")
