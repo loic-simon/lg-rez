f55c59ee95948e71b3fbc1926ed66de12dd9eae5
Tom Lacoma <lacoma.tom@gmail.com>
2020-05-20 10:25:27 +0200

    Demande de chambre ds inscription

--- Discord/features/inscription.py
+++ Discord/features/inscription.py
@@ -2,6 +2,11 @@ from discord.ext import commands
 import tools
 from bdd_connect import db, cache_TDB
 
+trigYesNo = {"oui","non","o","n","yes","no","y","n"}
+
+repOui = {"oui","o","yes","y"}
+repNon =
+
 async def main(bot, member):
     if chan := tools.get(member.guild.text_channels, topic=f"{member.id}"):
         await chan.send(f"Tu as déjà un channel à ton nom, {member.mention}")
@@ -15,7 +20,7 @@ async def main(bot, member):
 
     await chan.send(f"Bienvenue {member.mention}, laisse moi t'aider à t'inscrire !\n Pour commencer, qui es-tu ?")
 
-    def checkChan(m):
+    def checkChan(m): #Check que le message soit envoyé par l'utilisateur et dans son channel perso
         return m.channel == chan and m.author == member
 
     vraiNom = await bot.wait_for('message', check=checkChan)
@@ -24,3 +29,16 @@ async def main(bot, member):
 
     await chan.edit(name = f"conv-bot-{vraiNom.content}")
     await member.edit(nick = f"{vraiNom.content}")
+
+    def checkTrigChan(m): #Check que le message soit une reponse oui/non et qu'elle soit dans le bon channel et pas le bon auteur
+        return checkChan(m) and tools.checkTrig(m,trigYesNo)
+
+    await chan.send("Habite-tu à la rez ? (O/N)")
+    rep = await bot.wait_for('message', check=checkTrigChan)
+
+    if a_la_rez := rep.content.lower() in repOui:
+        chambre = (await bot.wait_for('message', check=checkChan)).contient
+    else:
+        chambre = "XXX (chambre MJ)"
+
+    await tools.log(member, f"A la rez = {a_la_rez} et chambre = {chambre}")
