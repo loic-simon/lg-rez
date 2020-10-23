7387010b77745acbcba4fe4059acabfad3ffd954
SIMON Loic <loic-l.simon@edf.fr>
2020-10-23 17:00:16 +0200

    spacing up

--- docs/autocog.py
+++ docs/autocog.py
@@ -4,15 +4,15 @@ from discord.ext.commands import Cog  # the class that needs modified documentat
 
 
 cognote = """
-.. note::
-    Cette classe est un **Cog**, *i.e.* un rassemblements de commandes.
+    .. note::
+        Cette classe est un **Cog**, *i.e.* un rassemblements de commandes.
 
-    L'ensemble des commandes qu'elle contient, créées par le décorateur :py:func:`@discord.ext.commands.command <discord.ext.commands.command>`, sont des objets
-    :py:class:`discord.ext.commands.Command` accessibles comme ``cog.cmd_name``.
+        L'ensemble des commandes qu'elle contient, créées par le décorateur :py:func:`@discord.ext.commands.command <discord.ext.commands.command>`, sont des objets
+        :py:class:`discord.ext.commands.Command` accessibles comme ``cog.cmd_name``.
 
-    Pour plus de lisiblité, seules les fonctions appellées lors de l'invoquation des commandes (:py:attr:`Command.callback <discord.ext.commands.Command.callback>`) sont décrites ci-après, mais toutes les méthodes de :py:class:`~discord.ext.commands.Command` sont évidemment accessibles.
+        Pour plus de lisiblité, seules les fonctions appellées lors de l'invoquation des commandes (:py:attr:`Command.callback <discord.ext.commands.Command.callback>`) sont décrites ci-après, mais toutes les méthodes de :py:class:`~discord.ext.commands.Command` sont évidemment accessibles.
 
-    Ces *callbacks* prennent comme premier argument ``ctx`` (:py:class:`discord.ext.commands.Context`), le **contexte d'invocation** de la commande. Cet argument est construit automatiquement par ``discord.py`` à l'appel de :py:meth:`Bot.process_commands <discord.ext.commands.Bot.process_commands>` ou :py:meth:`Bot.get_context <discord.ext.commands.Bot.get_context>`, puis passé au callback suivi des arguments entrés par l'utilisateur.
+        Ces *callbacks* prennent comme premier argument ``ctx`` (:py:class:`discord.ext.commands.Context`), le **contexte d'invocation** de la commande. Cet argument est construit automatiquement par ``discord.py`` à l'appel de :py:meth:`Bot.process_commands <discord.ext.commands.Bot.process_commands>` ou :py:meth:`Bot.get_context <discord.ext.commands.Bot.get_context>`, puis passé au callback suivi des arguments entrés par l'utilisateur.
 """
 
 
@@ -32,7 +32,7 @@ class CogDocumenter(ClassDocumenter):
             doc[0].append(cognote)
 
             for command in self.object.get_commands(self.object):
-                doc[0].append(f".. automethod:: {self.fullname}.{command.callback.__name__}.callback")
+                doc[0].append(f"\n    .. automethod:: {self.fullname}.{command.callback.__name__}.callback")
 
         print("YOUHOUHOUHOUOHU", doc, self, self.__dict__)
         return doc
